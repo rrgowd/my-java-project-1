0622fae6a2e0bfcb5d77c6ce82e909ba220bacd8
Your Name <you@example.com>
2026-02-22 17:14:33 +0530

    new

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,32 @@
 #!/bin/bash
 
+TOMCAT_PATH="/c/Users/User/Downloads/apache-tomcat-9.0.107/apache-tomcat-9.0.107"
+WAR_NAME="userapp.war"
+
+echo "Stopping Tomcat..."
+"$TOMCAT_PATH/bin/shutdown.sh"
+
+sleep 5
+
+echo "Cleaning old build..."
+rm -rf target
+
 echo "Building project..."
 mvn clean package
 
-echo "Copying WAR to Tomcat webapps..."
+if [ $? -ne 0 ]; then
+  echo "Build Failed ❌"
+  exit 1
+fi
 
-cp -f target/userapp.war /c/Users/User/Downloads/apache-tomcat-9.0.107/apache-tomcat-9.0.107/webapps/
+echo "Removing old WAR..."
+rm -rf "$TOMCAT_PATH/webapps/userapp"
+rm -f "$TOMCAT_PATH/webapps/$WAR_NAME"
 
-echo "WAR copied successfully."
-echo "Now restart Tomcat."
+echo "Copying WAR..."
+cp target/$WAR_NAME "$TOMCAT_PATH/webapps/"
+
+echo "Starting Tomcat..."
+"$TOMCAT_PATH/bin/startup.sh"
+
+echo "Deployment Successful ✅"
